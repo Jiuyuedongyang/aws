adaa05b91980487d8093e2105f8d79e4276d5be6
undefined <undefined>
2020-09-26 22:16:52 +0800

    更新了注释

--- pages/exam/index.js
+++ pages/exam/index.js
@@ -525,41 +525,6 @@ Page({
 
   },
 
-  getUserInfo: function () {
-    //判断用户是否授权
-    wx.getSetting({
-      success: (data) => {
-        console.log(data);
-        console.log("成功");
-        if (data.authSetting["scope.userInfo"]) {
-          //用户已经授权
-          this.setData({
-            isShow: false,
-          });
-        } else {
-          //没有授权
-          this.setData({
-            isShow: true,
-          });
-        }
-      },
-    });
-
-    //获取用户登录的信息
-    wx.getUserInfo({
-      success: (data) => {
-        console.log(data);
-
-        //更新data中userInfo
-        this.setData({
-          userInfo: data.userInfo,
-        });
-      },
-      fail: () => {
-        console.log("获取用户数据失败");
-      },
-    });
-  },
 
 
   /**
