63e9343d8bc91e544ffc517453f7267025a12539
undefined <undefined>
2020-09-29 22:23:19 +0800

    增加了wx.showToast

--- pages/saa/index.js
+++ pages/saa/index.js
@@ -4,7 +4,17 @@
 
 Page({
   data: {
-  
   },
 
+
+if_vip(){
+  wx.showToast({  
+    title: '请联系wx购买题库',  
+    icon: 'success',  
+    duration: 2000  
+})  
+
+    console.log("yes")
+  }
 });
+
