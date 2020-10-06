30bfa4c61b0c544cbf9a84da0111fccd36b48c15
undefined <undefined>
2020-10-06 13:32:17 +0800

    调整授权js

--- pages/cloud/index.js
+++ pages/cloud/index.js
@@ -2,34 +2,34 @@ const app = getApp();
 
 Page({
   data: {
-    msg: "mini program",
     userInfo: "",
     isShow: true,
   },
 
   
   handleGetUserInfo(data) {
-    console.log("用户点击了", data);
     //判断用户点击的是否是允许
-    if (data.detail.rawData) {
-      //用户点击的是允许
+    if (data.detail.rawData) {//如果存在信息说明之前已经允许过
       this.OnloadGetUserInfo();
     }
   },
+
+
   OnloadGetUserInfo() {
-    //判断用户是否授权
+    //先wx.getSetting()看用户是否授权
     wx.getSetting({
       success: (data) => {
-        console.log(data);
-        console.log("成功");
         if (data.authSetting["scope.userInfo"]) {
           //用户已经授权
           this.setData({
             isShow: false,
           });
+
+
+
+          
         } else {
           //没有授权
-          console.log("用户不授权")
           this.setData({
             isShow: true,
           });
@@ -40,16 +40,10 @@ Page({
     //获取用户登录的信息
     wx.getUserInfo({
       success: (data) => {
-        console.log(data);
-
-        //更新data中userInfo
         this.setData({
           userInfo: data.userInfo,
         });
-      },
-      fail: () => {
-        console.log("获取用户数据失败");
-      },
+      }
     });
   },
 
@@ -68,7 +62,6 @@ Page({
   // 生命周期函数--监听页面加载
   onLoad: function (options) {
     console.log("页面加载");
-    // console.log(this);
     this.OnloadGetUserInfo();
   },
 
