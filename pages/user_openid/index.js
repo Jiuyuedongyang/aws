8c769cf4ffa849a775ae8db3a9e9b0679c28d96a
undefined <undefined>
2020-10-09 10:17:32 +0800

    用户user_openid界面加入clearstorage函数

--- pages/user_openid/index.js
+++ pages/user_openid/index.js
@@ -3,15 +3,13 @@ const APP_SECRET = '750e4e14e963b56ffb61bf1ef24ced52'; //输入小程序app_secr
 var OPEN_ID = '' //储存获取到openid  
 var SESSION_KEY = '' //储存获取到session_key  
 Page({
-    data: {
-    }
-    ,
+    data: {},
     getOpenId: function () {
       var that = this;
       wx.login({
         success: function (res) {
           console.log(res)
-          console.log(res.code)//打印wx.login()返回的code码，通过code码发送给flask生成openid
+          console.log(res.code) //打印wx.login()返回的code码，通过code码发送给flask生成openid
           wx.request({
             url: 'https://aws.lycaicai.top:5001/get_openid',
             data: {
@@ -21,30 +19,35 @@ Page({
             success(res) {
               console.log("get")
               // console.log(res)
-              console.log(res.data)//res.data是flask返回的openid数据
-              let openid=res.data
-              console.log("openid= ",openid)
+              console.log(res.data) //res.data是flask返回的openid数据
+              let openid = res.data
+              console.log("openid= ", openid)
               that.setData({
-                openid:openid
+                openid: openid
               })
-              wx.setStorageSync('openid',openid)
+              wx.setStorageSync('openid', openid)
             }
-            
+
           })
 
 
-        
+
         }
       })
       // setTimeout(function () {
-        
+
       //   console.log("延迟函数")
-      
+
       //   console.log("延迟函数")
       //   //要延时执行的代码
       // }, 3000) //延迟时间 这里是2秒
+    },
+    clearstoragesync() {
+      wx.clearStorageSync()
+      wx.showToast({
+        title: '清除成功',
+      })
     }
-
 
 
 
