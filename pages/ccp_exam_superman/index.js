3c69ad73401f720a53c0678d15e49bb36fc76e70
undefined <undefined>
2020-10-10 21:16:43 +0800

    将saa改为ccp

--- pages/ccp_exam_superman/index.js
+++ pages/ccp_exam_superman/index.js
@@ -371,7 +371,7 @@ let that=this
       success(res) {
         if (res.confirm) {
 
-          let cleraAll = wx.getStorageSync('saa')
+          let cleraAll = wx.getStorageSync('ccp')
           console.log(cleraAll)
           that.setData({
             input_page: cleraAll.input_page, //用cl户跳转页面默认为1
@@ -470,54 +470,54 @@ let that=this
     });
   },
 
-  setsaa() {
-    let saa_body = wx.getStorageSync(
-      'saa_body',
+  setccp() {
+    let ccp_body = wx.getStorageSync(
+      'ccp_body',
     )
 
-    console.log(saa_body)
+    console.log(ccp_body)
     // success: (res) => {
-    //   console.log("saa_body_ok")
+    //   console.log("ccp_body_ok")
     //   console.log(res.data)
 
     // }
     this.setData({
-      body: saa_body
+      body: ccp_body
     })
     //此时本页面中的所有信息
     console.log("此=======")
     console.log(this.data)
     console.log("此=======")
 
-    //设置saa
-    wx.setStorageSync('saa', this.data)
+    //设置ccp
+    wx.setStorageSync('ccp', this.data)
   },
   /**
    * 生命周期函数--监听页面加载
    */
 
 
-  getsaa_current() {
-    let saa_current = wx.getStorageSync('saa_current')
-    console.log("saa_current get")
-    console.log(saa_current)
+  getccp_current() {
+    let ccp_current = wx.getStorageSync('ccp_current')
+    console.log("ccp_current get")
+    console.log(ccp_current)
 
-    if (saa_current) {
+    if (ccp_current) {
       this.setData({
 
 
 
 
-        input_page: saa_current.input_page, //用cl户跳转页面默认为1
-        outterIndex: saa_current.outterIndex, //题号索引 外索引
-        innerIndex: saa_current.innerIndex, //选项索引 内索引
-        flag: saa_current.flag, //答案标志位，用于 答错为0，答对为1，否则为任意数这里填100
-        currentTab: saa_current.currentTab, //当前swiper的页数
-        answerAll: saa_current.answerAll, //多选题数组
-        count_right: saa_current.count_right, //已答题正确数
-        count_sum: saa_current.count_sum, //已答总数
-        right_rate: saa_current.right_rate, //正确率
-        body: saa_current.body
+        input_page: ccp_current.input_page, //用cl户跳转页面默认为1
+        outterIndex: ccp_current.outterIndex, //题号索引 外索引
+        innerIndex: ccp_current.innerIndex, //选项索引 内索引
+        flag: ccp_current.flag, //答案标志位，用于 答错为0，答对为1，否则为任意数这里填100
+        currentTab: ccp_current.currentTab, //当前swiper的页数
+        answerAll: ccp_current.answerAll, //多选题数组
+        count_right: ccp_current.count_right, //已答题正确数
+        count_sum: ccp_current.count_sum, //已答总数
+        right_rate: ccp_current.right_rate, //正确率
+        body: ccp_current.body
 
 
       })
@@ -531,7 +531,7 @@ let that=this
 
   onLoad: function () {
     this.adapt_screen()
-    this.setsaa()
+    this.setccp()
 
 
 
@@ -550,7 +550,7 @@ let that=this
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.getsaa_current()
+    this.getccp_current()
   },
 
   /**
@@ -566,7 +566,7 @@ let that=this
    */
   onUnload: function () {
     console.log("onunload")
-    wx.setStorageSync('saa_current', this.data)
+    wx.setStorageSync('ccp_current', this.data)
     console.log(this.data)
     console.log("onunload")
   },
