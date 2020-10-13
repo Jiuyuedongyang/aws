8b9cf6ee9145a8f18212925a15d55633e777cf59
undefined <undefined>
2020-10-13 10:49:51 +0800

    修改了加载时间 格式化了文档

--- pages/saa_exam_superman/index.js
+++ pages/saa_exam_superman/index.js
@@ -349,10 +349,10 @@ Page({
     console.log(e)
     let toggle = this.data.body[this.data.currentTab].en_cn;
     // console.log(toggle);
-    if(toggle==0){
-      toggle=1
-    }else if(toggle==1){
-      toggle=0
+    if (toggle == 0) {
+      toggle = 1
+    } else if (toggle == 1) {
+      toggle = 0
     }
     // console.log(toggle);
     console.log(this.data.outterIndex, this.data.innerIndex)
@@ -364,7 +364,7 @@ Page({
     // console.log("aa");
   },
   clearAll() {
-let that=this
+    let that = this
     wx.showModal({
       title: '是否重置',
       content: '该操作不可撤销',
@@ -422,10 +422,10 @@ let that=this
     console.log(this.data.body[this.data.currentTab].isCollected)
     let isCollected = this.data.body[this.data.currentTab].isCollected
 
-    if(isCollected==0){
-      isCollected=1
-    }else if(isCollected==1){
-      isCollected=0
+    if (isCollected == 0) {
+      isCollected = 1
+    } else if (isCollected == 1) {
+      isCollected = 0
     }
     console.log(isCollected)
     let str = "body[" + this.data.currentTab + "].isCollected";
@@ -434,20 +434,20 @@ let that=this
     });
 
 
-//
-    if (isCollected==true){
+    //
+    if (isCollected == true) {
       wx.showToast({
-        title:'收藏成功',
+        title: '收藏成功',
         icon: 'success'
       })
-    }else{
+    } else {
       wx.showToast({
-        title:'取消收藏',
-        icon:'none'
+        title: '取消收藏',
+        icon: 'none'
       })
     }
     //提示用户
-    
+
     console.log("123---321")
     console.log(this.data)
     console.log("123---321")
@@ -532,7 +532,12 @@ let that=this
   onLoad: function () {
     this.adapt_screen()
     this.setsaa()
-
+    wx.showToast({
+      title: '加载中...6s', //提示文字
+      duration: 6000, //显示时长
+      mask: true, //是否显示透明蒙层，防止触摸穿透，默认：false  
+      icon: 'loading', //图标，支持"success"、"loading"  
+    })
 
 
 
